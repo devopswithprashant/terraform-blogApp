c15c2a111642177d332426ffb81b2450440635ae
devopswithprashant <devopswithprashant@gmail.com>
2026-05-02 17:58:13 +0530

    feat: create db & network resources from terraform project.

--- data.tf
+++ data.tf
@@ -1,49 +1,52 @@
-data "aws_region" "current" {}
+# data "aws_region" "current" {}
 
-data "aws_vpc" "nonprod_blog" {
-  tags = {
-    Name = "nonprod-blog-vpc"
-    Type = "nonprod"
-  }
-}
+data "aws_caller_identity" "current" {}
+data "aws_availability_zones" "available" {}
 
-data "aws_subnets" "nonprod_blog_subnets" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.nonprod_blog.id]
-  }
+# data "aws_vpc" "nonprod_blog" {
+#   tags = {
+#     Name = "nonprod-blog-vpc"
+#     Type = "nonprod"
+#   }
+# }
 
-  tags = {
-    Type = "Private"
-  }
-}
+# data "aws_subnets" "nonprod_blog_subnets" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.nonprod_blog.id]
+#   }
 
-data "aws_subnets" "nonprod_blog_subnets_public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.nonprod_blog.id]
-  }
+#   tags = {
+#     Type = "Private"
+#   }
+# }
 
-  tags = {
-    Type = "Public"
-  }
-}
+# data "aws_subnets" "nonprod_blog_subnets_public" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.nonprod_blog.id]
+#   }
+
+#   tags = {
+#     Type = "Public"
+#   }
+# }
 
 
 data "aws_key_pair" "jumpserver_key" {
-  key_name = "testing"
+  key_name = "bastion-host-key"
 }
 
-# Ubuntu AMI data source
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
-#   owners      = ["099720109477"] # Canonical
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-*-server-*"]
-#   }
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-# }
+# # Ubuntu AMI data source
+# # data "aws_ami" "ubuntu" {
+# #   most_recent = true
+# #   owners      = ["099720109477"] # Canonical
+# #   filter {
+# #     name   = "name"
+# #     values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-*-server-*"]
+# #   }
+# #   filter {
+# #     name   = "virtualization-type"
+# #     values = ["hvm"]
+# #   }
+# # }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,20 @@
+variable "enable_rds_bootstrap_ssm" {
+  type        = bool
+  description = "After RDS is created, run an SSM document on the bastion to create app DB, roles, and schemas."
+  default     = true
+}
+
 locals {
-  name = "blogapp-cluster"
+  name    = "dwp-platform-nonprod"
+  region  = "eu-west-1"
+  region2 = "eu-central-1"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Name       = local.name
+    Example    = local.name
+    Project    = "DevOps with Prashant Platform"
+  }
 }
